0c8bd2a1030c451df2af90267bb17fbb97bfec7c
mai-repo <ttmai@my.loyno.edu>
2025-01-08 22:48:53 -0500

    created a templates folder

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# Line imports flas and render_template function from the flask module
 from flask import Flask, render_template
 
+# Create an instance of the Flask class and assign it to the variable app
 app = Flask(__name__)
 
+# Define a route for the root URL of the website
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') # Return the rendered template
 
+# Define a route for the about page
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Run the app in debug mode
